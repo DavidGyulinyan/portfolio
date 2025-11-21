3adab4fafaecfd7db17b767d5dc4f7dd196fab9f
davit <davidgyulinyan@gmail.com>
2025-11-22 00:21:19 +0400

    update: texts

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,8 +8,8 @@ const content = {
     socialLinks: "Follow me on social media",
   },
   hy: {
-    getInTouch: "Կապվեք ինձ հետ",
-    socialLinks: "Հետևեք ինձ սոցիալական մեդիայով",
+    getInTouch: "Կապվեք",
+    socialLinks: "Կապվեք ինձ հետ սոցիալական հարթակներով, կամ ծանոթացեք իմ նախագծերին GitHub-ում",
   },
 };
 
